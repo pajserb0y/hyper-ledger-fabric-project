ed8d8e938f2a718217e97a7dcbd297b0d588952d
marko <nikmarko555@gmail.com>
2023-02-20 02:41:06 +0100

    change car color

--- chaincode/cars/go/cars.go
+++ chaincode/cars/go/cars.go
@@ -17,7 +17,6 @@ type SmartContract struct {
 	contractapi.Contract
 }
 
-
 type Owner struct {
 	Id      int     `json:"id"`
 	Name    string  `json:"name"`
@@ -26,81 +25,96 @@ type Owner struct {
 	Money   float64 `json:"money"`
 }
 
-type Malfunction struct {
+type Damage struct {
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
 }
 
-// Car describes basic details of what makes up a car
 type Car struct {
-	Id	   int     `json:"id"`
-	Make   string  `json:"make"`
-	Model  string  `json:"model"`
-	Colour string  `json:"colour"`
-	Year   string  `json:"year"`
-	Owner  string  `json:"owner"`
-	Price  float64 `json:"price"`
+	Id           int           `json:"id"`
+	Make         string        `json:"make"`
+	Model        string        `json:"model"`
+	Color        string        `json:"color"`
+	Owner        string        `json:"owner"`
+	Damages 	[]Damage 	   `json:"damages"`
+	Price        float64       `json:"price"`
 }
 
-// QueryResult structure used for handling result of query
 type QueryResult struct {
 	Key    string `json:"Key"`
 	Record *Car
 }
 
-// InitLedger adds a base set of cars to the ledger
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	owners := []Owner{
-		{Id: 1, Name: "Marko", Surname: "Nikolic", Email: "markonikolic@gmail.com", Money: 8000},
-		{Id: 2, Name: "NIkola", Surname: "Kajtes", Email: "nikolakajtes@gmail.com", Money: 11000},
-	}
-
 	cars := []Car{
-		{Id: 1, Make: "Toyota", Model: "Prius", Colour: "blue",Year: "2020", Owner: "1",
-			Malfunctions: []Malfunction{
-				{Description: "Bad engine", Price: 250},
+		{Id: 1, Make: "Toyota", Model: "Prius", Color: "blue", Owner: "1",
+			Damages: []Damage{
+				{Description: "Bad engine", Price: 1000},
 			},
 			Price: 5000,
 		},
-		{Id: 2, Make: "Ford", Model: "Mustang", Colour: "blue",Year: "2010", Owner: "1", Malfunctions: []Malfunction{}, Price: 3000},
-		{Id: 3, Make: "Hyundai", Model: "Tucson", Colour: "green",Year: "2021", Owner: "2",
-			Malfunctions: []Malfunction{
-				{Description: "Eletrionic failure", Price: 1000},
+		{Id: 2, Make: "Ford", Model: "Mustang", Color: "blue", Owner: "3", Damages: []Damage{}, Price: 3000},
+		{Id: 3, Make: "Hyundai", Model: "Tucson", Color: "green", Owner: "2",
+			Damages: []Damage{
+				{Description: "Eletronic failure", Price: 800},
 			},
 			Price: 2500,
 		},
-		{Id: 4, Make: "Volkswagen", Model: "Passat", Colour: "blue",Year: "2020", Owner: "2", Malfunctions: []Malfunction{}, Price: 7000},
-		{Id: 5, Make: "Tesla", Model: "S", Colour: "blue",Year: "2023", Owner: "1", Malfunctions: []Malfunction{}, Price: 20000},
+		{Id: 4, Make: "Volkswagen", Model: "Passat", Color: "blue", Owner: "2", Damages: []Damage{}, Price: 7000},
+		{Id: 5, Make: "Tesla", Model: "S", Color: "blue", Owner: "3", Damages: []Damage{}, Price: 20000},
+		{Id: 6, Make: "Peugeot", Model: "205", Color: "black", Owner: "3", Damages: []Damage{}, Price: 2000},
 	}
 
-	for i, car := range cars {
-		carAsBytes, _ := json.Marshal(car)
-		err := ctx.GetStub().PutState("CAR"+strconv.Itoa(i), carAsBytes)
+	owners := []Owner{
+		{Id: 1, Name: "Marko", Surname: "Markovic", Email: "markonikolic@gmail.com", Money: 80000},
+		{Id: 2, Name: "Nikola", Surname: "Kajtes", Email: "nikolakajtes@gmail.com", Money: 100000},
+		{Id: 3, Name: "Petar", Surname: "Nikolic", Email: "petarnikolic@gmail.com", Money: 150000},
+	}
 
+	for _, car := range cars {
+		carAsBytes, err := json.Marshal(car)
 		if err != nil {
-			return fmt.Errorf("Failed to put to world state. %s", err.Error())
+			return err
+		}
+
+		err = ctx.GetStub().PutState("CAR"+strconv.Itoa(car.Id), carAsBytes)
+		if err != nil {
+			return fmt.Errorf("failed to put car to world state. %v", err)
+		}
+
+		indexName := "color~owner~id"
+		colorOwnerIndexKey, err := ctx.GetStub().CreateCompositeKey(indexName, []string{car.Color, car.Owner, strconv.Itoa(car.Id)})
+		if err != nil {
+			return err
+		}
+
+		value := []byte{0x00}
+		err = ctx.GetStub().PutState(colorOwnerIndexKey, value)
+		if err != nil {
+			return err
 		}
 	}
 
 	for _, owner := range owners {
-	ownerAsBytes, _ := json.Marshal(owner)
-	err := ctx.GetStub().PutState("OWNER"+strconv.Itoa(owner.Id), ownerAsBytes)
+		ownerAsBytes, _ := json.Marshal(owner)
+		err := ctx.GetStub().PutState("OWNER"+strconv.Itoa(owner.Id), ownerAsBytes)
 
-	if err != nil {
-		return fmt.Errorf("failed to put to world state. %s", err.Error())
+		if err != nil {
+			return fmt.Errorf("failed to put to world state. %s", err.Error())
 		}
 	}
-	
+
 	return nil
 }
 
 // CreateCar adds a new car to the world state with given details
-func (s *SmartContract) CreateCar(ctx contractapi.TransactionContextInterface, carNumber string, make string, model string, colour string, owner string) error {
+func (s *SmartContract) CreateCar(ctx contractapi.TransactionContextInterface, carNumber string, make string, model string, color string, owner string, price float64) error {
 	car := Car{
-		Make:   make,
-		Model:  model,
-		Colour: colour,
-		Owner:  owner,
+		Make:  make,
+		Model: model,
+		Color: color,
+		Owner: owner,
+		Price: price,
 	}
 
 	carAsBytes, _ := json.Marshal(car)
@@ -108,16 +122,15 @@ func (s *SmartContract) CreateCar(ctx contractapi.TransactionContextInterface, c
 	return ctx.GetStub().PutState(carNumber, carAsBytes)
 }
 
-// QueryCar returns the car stored in the world state with given id
-func (s *SmartContract) QueryCar(ctx contractapi.TransactionContextInterface, carNumber string) (*Car, error) {
-	carAsBytes, err := ctx.GetStub().GetState(carNumber)
+func (s *SmartContract) GetCarById(ctx contractapi.TransactionContextInterface, carId string) (*Car, error) {
 
+	carAsBytes, err := ctx.GetStub().GetState(carId)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to read from world state. %s", err.Error())
+		return nil, fmt.Errorf("failed to read from world state. %s", err.Error())
 	}
 
 	if carAsBytes == nil {
-		return nil, fmt.Errorf("%s does not exist", carNumber)
+		return nil, fmt.Errorf("%s does not exist", carId)
 	}
 
 	car := new(Car)
@@ -126,50 +139,231 @@ func (s *SmartContract) QueryCar(ctx contractapi.TransactionContextInterface, ca
 	return car, nil
 }
 
-// QueryAllCars returns all cars found in world state
-func (s *SmartContract) QueryAllCars(ctx contractapi.TransactionContextInterface) ([]QueryResult, error) {
-	startKey := ""
-	endKey := ""
+func (s *SmartContract) GetOwnerById(ctx contractapi.TransactionContextInterface, ownerId string) (*Owner, error) {
+	ownerAsBytes, err := ctx.GetStub().GetState(ownerId)
 
-	resultsIterator, err := ctx.GetStub().GetStateByRange(startKey, endKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from world state. %s", err.Error())
+	}
 
+	if ownerAsBytes == nil {
+		return nil, fmt.Errorf("%s does not exist", ownerId)
+	}
+
+	owner := new(Owner)
+	_ = json.Unmarshal(ownerAsBytes, owner)
+
+	return owner, nil
+}
+
+func (s *SmartContract) GetAllCars(ctx contractapi.TransactionContextInterface) ([]*Car, error) {
+	// range query with empty string for startKey and endKey does an
+	// open-ended query of all assets in the chaincode namespace.
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
 	if err != nil {
 		return nil, err
 	}
 	defer resultsIterator.Close()
 
-	results := []QueryResult{}
-
+	var cars []*Car
 	for resultsIterator.HasNext() {
 		queryResponse, err := resultsIterator.Next()
-
 		if err != nil {
 			return nil, err
 		}
 
-		car := new(Car)
-		_ = json.Unmarshal(queryResponse.Value, car)
-
-		queryResult := QueryResult{Key: queryResponse.Key, Record: car}
-		results = append(results, queryResult)
+		var car Car
+		err = json.Unmarshal(queryResponse.Value, &car)
+		if err != nil {
+			return nil, err
+		}
+		cars = append(cars, &car)
 	}
 
-	return results, nil
+	return cars, nil
 }
 
-// ChangeCarOwner updates the owner field of car with given id in world state
-func (s *SmartContract) ChangeCarOwner(ctx contractapi.TransactionContextInterface, carNumber string, newOwner string) error {
-	car, err := s.QueryCar(ctx, carNumber)
-
+func (s *SmartContract) ChangeCarColor(ctx contractapi.TransactionContextInterface, carId string, color string) error {
+	car, err := s.GetCarById(ctx, carId)
 	if err != nil {
 		return err
 	}
 
-	car.Owner = newOwner
+	oldKey, _ := ctx.GetStub().CreateCompositeKey("color~owner~id", []string{car.Color, car.Owner, strconv.Itoa(car.Id)})
 
+	car.Color = color
 	carAsBytes, _ := json.Marshal(car)
+	err = ctx.GetStub().PutState(carId, carAsBytes)
+	if err != nil {
+		return err
+	}
 
-	return ctx.GetStub().PutState(carNumber, carAsBytes)
+	key, err := ctx.GetStub().CreateCompositeKey("color~owner~id", []string{color, car.Owner, strconv.Itoa(car.Id)})
+	if err != nil {
+		return err
+	}
+
+	ctx.GetStub().DelState(oldKey)
+	value := []byte{0x00}
+	err = ctx.GetStub().PutState(key, value)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+
+func (s *SmartContract) TransferOwnership(ctx contractapi.TransactionContextInterface, carId string, newOwner string, acceptsDamages bool) error {
+
+	carExists, err := s.CarExists(ctx, carId)
+	if err != nil {
+		return fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	ownerExists, err := s.OwnerExists(ctx, newOwner)
+	if err != nil {
+		return fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	if carExists && ownerExists {
+		car, err := s.GetCarById(ctx, carId)
+		if err != nil {
+			fmt.Println("Car with specified id does not exist")
+			return err
+		}
+
+		owner, err := s.GetOwnerById(ctx, newOwner)
+		if err != nil {
+			fmt.Println("Owner with specified id does not exist")
+			return err
+		}
+
+		if len(car.Damages) == 0 && owner.Money >= car.Price {
+
+			oldOwnerId := car.Owner
+
+			oldKey, _ := ctx.GetStub().CreateCompositeKey("color~owner~id", []string{car.Color, car.Owner, carId})
+			car.Owner = strconv.Itoa(owner.Id)
+			carAsBytes, _ := json.Marshal(car)
+			ctx.GetStub().PutState(carId, carAsBytes)
+
+			key, err := ctx.GetStub().CreateCompositeKey("color~owner~id", []string{car.Color, strconv.Itoa(owner.Id), strconv.Itoa(car.Id)})
+			if err != nil {
+				return err
+			}
+
+			ctx.GetStub().DelState(oldKey)
+			value := []byte{0x00}
+			err = ctx.GetStub().PutState(key, value)
+			if err != nil {
+				return err
+			}
+
+			ownerMoney := owner.Money - car.Price
+			owner.Money = ownerMoney
+			ownerAsBytes, _ := json.Marshal(owner)
+			ctx.GetStub().PutState(newOwner, ownerAsBytes)
+
+			oldOwner, err := s.GetOwnerById(ctx, "OWNER"+oldOwnerId)
+			if err != nil {
+				fmt.Println("Owner with specified id does not exist")
+				return err
+			}
+
+			oldOwner.Money = oldOwner.Money + car.Price
+			oldOwnerAsBytes, _ := json.Marshal(oldOwner)
+			ctx.GetStub().PutState("OWNER"+oldOwnerId, oldOwnerAsBytes)
+
+		} else if len(car.Damages) > 0 && acceptsDamages {
+			damagesPrice := 0.0
+			for _, damage := range car.Damages {
+				damagesPrice += damage.Price
+			}
+
+			carPrice := car.Price - damagesPrice
+
+			if owner.Money >= carPrice {
+				oldOwnerId := car.Owner
+
+				oldKey, _ := ctx.GetStub().CreateCompositeKey("color~owner~id", []string{car.Color, car.Owner, carId})
+				car.Owner = strconv.Itoa(owner.Id)
+				carAsBytes, _ := json.Marshal(car)
+				ctx.GetStub().PutState(carId, carAsBytes)
+
+				key, err := ctx.GetStub().CreateCompositeKey("color~owner~id", []string{car.Color, strconv.Itoa(owner.Id), strconv.Itoa(car.Id)})
+				if err != nil {
+					return err
+				}
+
+				ctx.GetStub().DelState(oldKey)
+				value := []byte{0x00}
+				err = ctx.GetStub().PutState(key, value)
+				if err != nil {
+					return err
+				}
+
+				ownerMoney := owner.Money - carPrice
+				owner.Money = ownerMoney
+				ownerAsBytes, _ := json.Marshal(owner)
+				ctx.GetStub().PutState(newOwner, ownerAsBytes)
+
+				oldOwner, err := s.GetOwnerById(ctx, "OWNER"+oldOwnerId)
+				if err != nil {
+					fmt.Println("Owner with specified id does not exist")
+					return err
+				}
+
+				oldOwner.Money = oldOwner.Money + carPrice
+				oldOwnerAsBytes, _ := json.Marshal(oldOwner)
+				ctx.GetStub().PutState("OWNER"+oldOwnerId, oldOwnerAsBytes)
+
+			} else {
+				return fmt.Errorf("new owner does not have enough money to buy this car")
+			}
+		} else if len(car.Damages) > 0 && !acceptsDamages {
+
+			return fmt.Errorf("car has damages and new owner does not want them")
+
+		} else if len(car.Damages) == 0 && owner.Money < car.Price {
+
+			return fmt.Errorf("new owner does not have enough money to buy this car")
+		}
+
+	}
+
+	return nil
+}
+
+func (s *SmartContract) CarExists(ctx contractapi.TransactionContextInterface, carId string) (bool, error) {
+	car, err := ctx.GetStub().GetState(carId)
+	if err != nil {
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	return car != nil, nil
+}
+
+func (s *SmartContract) OwnerExists(ctx contractapi.TransactionContextInterface, ownerId string) (bool, error) {
+	owner, err := ctx.GetStub().GetState(ownerId)
+	if err != nil {
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	return owner != nil, nil
+}
+
+func (s *SmartContract) DeleteCar(ctx contractapi.TransactionContextInterface, carId string) error {
+	exists, err := s.CarExists(ctx, carId)
+	if err != nil {
+		return fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	if !exists {
+		return fmt.Errorf("the asset %s does not exist", carId)
+	}
+
+	return ctx.GetStub().DelState(carId)
 }
 
 func main() {
